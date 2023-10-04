e055a34c9eb98b5fbee609803415a3413a77348f
altafNEU <shaikh.alt@northeastern.edu>
2023-10-04 13:19:13 -0400

    health test

--- tests/integration/healthz.test.js
+++ tests/integration/healthz.test.js
@@ -1,3 +1,26 @@
+
+import request from "supertest";
+import express from "express";
+import bodyParser from "body-parser";
+//import router from "../routes/healthz.route.js";
+import router from "../../routes/app.route.js";
+import * as dotenv from 'dotenv';
+dotenv.config()
+
+const app = express();
+app.use(bodyParser.json());
+app.use(router);
+describe("Healthz Endpoint", () => {
+  it("Return 200 if successful GET requests without body or query", async () => {
+    // Mock successful DB connection for this test
+    jest.mock("../../models/index.js" , () => ({
+      connectionTest: () => Promise.resolve(),
+    }));
+    const res = await request(app).get("/healthz");
+    expect(res.statusCode).toEqual(200);
+  });
+});
+
 //   import request from "supertest";
 //   import router from "../../app.js"; // Import your Express app instance from your main app file
 //   import express from "express";
@@ -16,12 +39,21 @@
 //       expect(res.statusCode).toEqual(200);
 //     });
 //   });
-import request from "supertest";
-import app from "../../app.js"; // Import your Express app instance from your main app file
 
-describe("Healthz Endpoint", () => {
-  it("should return 200 for successful GET requests without body or query", async () => {
-    const res = await request(app).get("/healthz");
-    expect(res.statusCode).toEqual(200);
-  });
-});
+
+
+// import request from "supertest";
+// import app from "../../app.js"; // Import your Express app instance from your main app file
+// import express from "express";
+// import bodyParser from "body-parser";
+// import * as dotenv from 'dotenv'
+// dotenv.config()
+
+// app.use(bodyParser.json());
+
+// describe("Healthz Endpoint", () => {
+//   it("should return 200 for successful GET requests without body or query", async () => {
+//     const res = await request(app).get("/healthz");
+//     expect(res.statusCode).toEqual(200);
+//   });
+// });
