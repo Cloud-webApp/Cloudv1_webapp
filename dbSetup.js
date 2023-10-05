2dc09b9aa30c70ea3fefb181b92609f14059b83c
altafNEU <shaikh.alt@northeastern.edu>
2023-10-04 20:29:26 -0400

    added coments on code

--- dbSetup.js
+++ dbSetup.js
@@ -4,72 +4,27 @@ import { Sequelize, DataTypes } from "sequelize";
 import insertDataFromCSV from "./csv-parser.js";
 import assignmentsModel from "./models/assignments.js";
 
-// Authenticate the Sequelize connection
-sequelize.authenticate()
-  .then(() => {
-    console.log("Connected to the database");
-  })
-  .catch((err) => {
-    console.error("Error while connecting to the db", err);
-  });
 
-// Create a database object
-const db = {};
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
-db.users = userModel(sequelize, DataTypes);
-db.assignments = assignmentsModel(sequelize, DataTypes);
+sequelize.authenticate().then(()=>{
+    console.log("Connected to the database")
+}).catch(err=>{
+    console.error("Error while connecting to the db", err)
+})
 
-// Define a foreign key relationship between users and assignments
-db.users.hasMany(db.assignments, {
-  foreignKey: { name: "user_id" },
-  onDelete: "CASCADE",
-  field: "user_id",
-  allowNull: false,
+const db={};
+db.Sequelize= Sequelize
+db.sequelize=sequelize;
+db.users= userModel(sequelize,DataTypes)
+db.assignments = assignmentsModel(sequelize,DataTypes)
+
+db.users.hasMany(db.assignments,{foreignKey:{name :"user_id"},onDelete:"CASCADE",field:"user_id",allowNull:false})
+
+db.sequelize.sync({force:false}).then(()=>{
+    console.log("yes re-sync done!")
+    insertDataFromCSV()
 });
 
-// Synchronize the database and insert data from CSV
-db.sequelize.sync({ force: false })
-  .then(() => {
-    console.log("Database synchronization completed!");
-    insertDataFromCSV();
-  });
 
 export default db;
-
-
-
-
-
-
-//draft below, above is cleaned with comments
-// import userModel from './models/users.js';
-// import sequelize from "./sequelize.js";
-// import { Sequelize, DataTypes } from "sequelize";
-// import insertDataFromCSV from "./csv-parser.js";
-// import assignmentsModel from "./models/assignments.js";
-
-
-// sequelize.authenticate().then(()=>{
-//     console.log("Connected to the database")
-// }).catch(err=>{
-//     console.error("Error while connecting to the db", err)
-// })
-
-// const db={};
-// db.Sequelize= Sequelize
-// db.sequelize=sequelize;
-// db.users= userModel(sequelize,DataTypes)
-// db.assignments = assignmentsModel(sequelize,DataTypes)
-
-// db.users.hasMany(db.assignments,{foreignKey:{name :"user_id"},onDelete:"CASCADE",field:"user_id",allowNull:false})
-
-// db.sequelize.sync({force:false}).then(()=>{
-//     console.log("yes re-sync done!")
-//     insertDataFromCSV()
-// });
-
-
-// export default db;
 
 
--- routes/assignment.route.js
+++ routes/assignment.route.js
@@ -1,260 +1,106 @@
-import { Router } from 'express';
-import db from '../dbSetup.js';
-import basicAuthenticator from '../middleware/basicAuthenticator.js';
+import {Router} from 'express';
+import db from "../dbSetup.js";
+import basicAuthenticator from "../middleware/basicAuthenticator.js";
 import _ from 'lodash';
-import assignmentValidator from '../validators/assignment.validator.js';
+import assignmentValidator from "../validators/assignment.validator.js";
 
-const assignmentRouter = Router();
-const assignmentDb = db.assignments;
+const assignmentRouter= Router();
+const assignmentDb=db.assignments;
 
-// Get a list of assignments
-assignmentRouter.get('/', async (req, res) => {
-  try {
-    const assignmentList = await assignmentDb.findAll({
-      attributes: { exclude: ['user_id'] },
-    });
+assignmentRouter.get("/", async (req,res)=>{
+     const assignmentList= await assignmentDb.findAll({attributes: { exclude: ['user_id'] }});
     console.log(req.authUser);
     res.status(200).json(assignmentList);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
 });
 
-// Get an assignment by ID
-assignmentRouter.get('/:id', async (req, res) => {
-  try {
-    const { id: assignmentId } = req.params;
-    const assignmentInfo = await db.assignments.findOne({
-      attributes: { exclude: ['user_id'] },
-      where: { assignment_id: assignmentId },
-    });
-    if (_.isEmpty(assignmentInfo)) {
-      return res.status(400).send('Assignment not found');
-    } else {
-      res.status(200).json(assignmentInfo);
+
+assignmentRouter.get("/:id",async (req,res)=>{
+    const {id:assignmentId}=req.params;
+    const assignmentInfo=await db.assignments.findOne({attributes: { exclude: ['user_id'] },where:{assignment_id:assignmentId}});
+    if(_.isEmpty(assignmentInfo) ){
+         return res.status(400).send();
+    }else {
+        res.status(200).json(assignmentInfo);
     }
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
+
 });
 
-// Create a new assignment
-assignmentRouter.post('/', basicAuthenticator, async (req, res) => {
-  try {
-    let { name, points, num_of_attemps, deadline } = req.body;
 
-    const { isError: isNotValid, errorMessage } =
-      assignmentValidator.validatePostRequest(req);
-    if (isNotValid) {
-      return res.status(400).json({ errorMessage });
+assignmentRouter.post("/",basicAuthenticator, async (req,res)=>{
+
+    let {name,points,num_of_attemps,deadline}=req.body;
+    
+    const {isError:isNotValid,errorMessage}=assignmentValidator.validatePostRequest(req);
+    if(isNotValid){
+        return res.status(400).json({errorMessage});
     }
 
-    const tempAssignment = {
-      name,
-      points,
-      num_of_attemps,
-      deadline,
-      user_id: req?.authUser?.user_id,
-    };
+    const tempAssignment={
+        name,points,num_of_attemps,deadline,
+        user_id:req?.authUser?.user_id,
+    }
 
-    const newAssignment = await assignmentDb.create(tempAssignment);
+    const newAssignment= await assignmentDb.create(tempAssignment);
     console.log(newAssignment);
     res.status(201).json(newAssignment);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
 });
 
-// Delete an assignment by ID
-assignmentRouter.delete('/:id', basicAuthenticator, async (req, res) => {
-  try {
-    const { id: assignmentId } = req.params;
-    const assignmentInfo = await db.assignments.findOne({
-      where: { assignment_id: assignmentId },
-    });
-    if (_.isEmpty(assignmentInfo)) {
-      return res.status(400).send('Assignment not found');
-    } else if (assignmentInfo.user_id !== req?.authUser?.user_id) {
-      return res.status(401).json({ error: 'You are not an authorized user' });
+
+assignmentRouter.delete("/:id",basicAuthenticator,async (req,res)=>{
+
+    const {id:assignmentId}=req.params;
+    const assignmentInfo=await db.assignments.findOne({where:{assignment_id:assignmentId}});
+    if(_.isEmpty(assignmentInfo) ){
+         return res.status(400).send();
+    }else if(assignmentInfo.user_id !== req?.authUser?.user_id){
+        return res.status(401).json({error:"Your are not authorized user"})
     }
 
-    await db.assignments.destroy({ where: { assignment_id: assignmentId } });
+    await db.assignments.destroy({where:{assignment_id:assignmentId}});
 
-    return res.status(204).send('Resource deleted successfully');
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
+    return res.status(204).json();
 });
 
-// Update an assignment by ID
-assignmentRouter.put('/:id', basicAuthenticator, async (req, res) => {
-  try {
-    const { id: assignmentId } = req.params;
+assignmentRouter.put("/:id",basicAuthenticator,async (req,res)=>{
 
-    const { isError: isNotValid, errorMessage } =
-      assignmentValidator.validateUpdateRequest(req);
-    if (isNotValid) {
-      return res.status(400).json({ errorMessage });
+    const {id:assignmentId}=req.params;
+
+    const {isError:isNotValid,errorMessage}=assignmentValidator.validateUpdateRequest(req);
+    if(isNotValid){
+        return res.status(400).json({errorMessage});
     }
 
-    const assignmentInfo = await db.assignments.findOne({
-      where: { assignment_id: assignmentId },
-    });
-    if (_.isEmpty(assignmentInfo)) {
-      return res.status(400).send('Assignment not found');
-    } else if (assignmentInfo.user_id !== req?.authUser?.user_id) {
-      return res.status(401).json({ error: 'You are not an authorized user' });
+    const assignmentInfo=await db.assignments.findOne({where:{assignment_id:assignmentId}});
+    if(_.isEmpty(assignmentInfo) ){
+         return res.status(400).send();
+    }else if(assignmentInfo.user_id !== req?.authUser?.user_id){
+        return res.status(401).json({error:"Your are not authorized user"})
     }
-
-    const { name, points, num_of_attemps, deadline } = req.body;
-
-    let updatedAssignment = {};
-
-    updatedAssignment = appendDataToObject(updatedAssignment, 'name', name);
-    updatedAssignment = appendDataToObject(updatedAssignment, 'points', points);
-    updatedAssignment = appendDataToObject(
-      updatedAssignment,
-      'num_of_attemps',
-      num_of_attemps
-    );
-    updatedAssignment = appendDataToObject(
-      updatedAssignment,
-      'deadline',
-      deadline
-    );
-
-    await db.assignments.update(updatedAssignment, {
-      where: { assignment_id: assignmentId },
-    });
-
-    return res.status(204).send('Resource updated successfully');
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-function appendDataToObject(object, field, value) {
-  if (!_.isNil(value)) {
-    object[field] = value;
-  }
-  return object;
-}
-
-export default assignmentRouter;
-
-
-
-
-//draft code, clean code is above 
-
-// import {Router} from 'express';
-// import db from "../dbSetup.js";
-// import basicAuthenticator from "../middleware/basicAuthenticator.js";
-// import _ from 'lodash';
-// import assignmentValidator from "../validators/assignment.validator.js";
-
-// const assignmentRouter= Router();
-// const assignmentDb=db.assignments;
-
-// assignmentRouter.get("/", async (req,res)=>{
-//      const assignmentList= await assignmentDb.findAll({attributes: { exclude: ['user_id'] }});
-//     console.log(req.authUser);
-//     res.status(200).json(assignmentList);
-// });
-
-
-// assignmentRouter.get("/:id",async (req,res)=>{
-//     const {id:assignmentId}=req.params;
-//     const assignmentInfo=await db.assignments.findOne({attributes: { exclude: ['user_id'] },where:{assignment_id:assignmentId}});
-//     if(_.isEmpty(assignmentInfo) ){
-//          return res.status(400).send();
-//     }else {
-//         res.status(200).json(assignmentInfo);
-//     }
-
-// });
-
-
-// assignmentRouter.post("/",basicAuthenticator, async (req,res)=>{
-
-//     let {name,points,num_of_attemps,deadline}=req.body;
+    const {name,points,num_of_attemps,deadline}= req.body;
     
-//     const {isError:isNotValid,errorMessage}=assignmentValidator.validatePostRequest(req);
-//     if(isNotValid){
-//         return res.status(400).json({errorMessage});
-//     }
-
-//     const tempAssignment={
-//         name,points,num_of_attemps,deadline,
-//         user_id:req?.authUser?.user_id,
-//     }
-
-//     const newAssignment= await assignmentDb.create(tempAssignment);
-//     console.log(newAssignment);
-//     res.status(201).json(newAssignment);
-// });
-
-
-// assignmentRouter.delete("/:id",basicAuthenticator,async (req,res)=>{
-
-//     const {id:assignmentId}=req.params;
-//     const assignmentInfo=await db.assignments.findOne({where:{assignment_id:assignmentId}});
-//     if(_.isEmpty(assignmentInfo) ){
-//          return res.status(400).send();
-//     }else if(assignmentInfo.user_id !== req?.authUser?.user_id){
-//         return res.status(401).json({error:"Your are not authorized user"})
-//     }
-
-//     await db.assignments.destroy({where:{assignment_id:assignmentId}});
-
-//     return res.status(204).json();
-// });
-
-// assignmentRouter.put("/:id",basicAuthenticator,async (req,res)=>{
-
-//     const {id:assignmentId}=req.params;
-
-//     const {isError:isNotValid,errorMessage}=assignmentValidator.validateUpdateRequest(req);
-//     if(isNotValid){
-//         return res.status(400).json({errorMessage});
-//     }
-
-//     const assignmentInfo=await db.assignments.findOne({where:{assignment_id:assignmentId}});
-//     if(_.isEmpty(assignmentInfo) ){
-//          return res.status(400).send();
-//     }else if(assignmentInfo.user_id !== req?.authUser?.user_id){
-//         return res.status(401).json({error:"Your are not authorized user"})
-//     }
-//     const {name,points,num_of_attemps,deadline}= req.body;
-    
-//     let updatedAssignment={
+    let updatedAssignment={
         
-//     }
-//     updatedAssignment=appendDataToObject(updatedAssignment,'name',name);
-//     updatedAssignment=appendDataToObject(updatedAssignment,'points',points);
-//     updatedAssignment=appendDataToObject(updatedAssignment,'num_of_attemps',num_of_attemps);
-//     updatedAssignment=appendDataToObject(updatedAssignment,'deadline',deadline);
+    }
+    updatedAssignment=appendDataToObject(updatedAssignment,'name',name);
+    updatedAssignment=appendDataToObject(updatedAssignment,'points',points);
+    updatedAssignment=appendDataToObject(updatedAssignment,'num_of_attemps',num_of_attemps);
+    updatedAssignment=appendDataToObject(updatedAssignment,'deadline',deadline);
    
     
-//     await db.assignments.update(updatedAssignment,{where:{assignment_id:assignmentId}});
+    await db.assignments.update(updatedAssignment,{where:{assignment_id:assignmentId}});
     
 
-//     return res.status(204).send('Resource deleted successfully');
+    return res.status(204).send('Resource deleted successfully');
 
-//    });
-
-
-// function appendDataToObject(object,field,value){
-//     if(!_.isNil(value)){
-//         object[field]=value;
-//     }
-//     return object;
-// }
+   });
 
 
-// export default assignmentRouter;
+function appendDataToObject(object,field,value){
+    if(!_.isNil(value)){
+        object[field]=value;
+    }
+    return object;
+}
+
+
+export default assignmentRouter;
