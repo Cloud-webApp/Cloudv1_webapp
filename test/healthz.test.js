469421a2413d033dde1dd3e7fba3a550185432c1
altafNEU <shaikh.alt@northeastern.edu>
2023-10-04 20:25:33 -0400

    added coments on code

--- test/healthz.test.js
+++ test/healthz.test.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(router);
 describe("Healthz Endpoint", () => {
-  it("should return 200 for successful GET requests without body or query", async () => {
+  it("returns a 200 status code on successful health check", async () => {
     // Mock successful DB connection for this test
     jest.mock("../dbSetup.js", () => ({
       connectionTest: () => Promise.resolve(),
@@ -20,40 +20,3 @@ describe("Healthz Endpoint", () => {
   });
 
 });
-
-//   import request from "supertest";
-//   import router from "../../app.js"; // Import your Express app instance from your main app file
-//   import express from "express";
-//   import bodyParser from "body-parser";
-//   import app from "../../app.js";
-//   import * as dotenv from 'dotenv'
-//   dotenv.config()
-//  const app = express();
-  
-// app.use(bodyParser.json());
-
-// app.use(router);
-//   describe("Healthz Endpoint", () => {
-//     it(" Return 200 for successful GET requests without body or query", async () => {
-//       const res = await request(app).get("/healthz");
-//       expect(res.statusCode).toEqual(200);
-//     });
-//   });
-
-
-
-// import request from "supertest";
-// import app from "../../app.js"; // Import your Express app instance from your main app file
-// import express from "express";
-// import bodyParser from "body-parser";
-// import * as dotenv from 'dotenv'
-// dotenv.config()
-
-// app.use(bodyParser.json());
-
-// describe("Healthz Endpoint", () => {
-//   it("should return 200 for successful GET requests without body or query", async () => {
-//     const res = await request(app).get("/healthz");
-//     expect(res.statusCode).toEqual(200);
-//   });
-// });
