83700ed79a969b505c02f5b783a078bc2898212b
altafNEU <shaikh.alt@northeastern.edu>
2023-10-05 17:04:46 -0400

    feature-10, removed unessary comments

--- routes/app.route.js
+++ routes/app.route.js
@@ -23,30 +23,3 @@ const checkHealth = async (req, res) => {
 
 export default checkHealth;
 
-
-
-
-//draft code, added comments above and else block for other requests
-// import sequelize from "../sequelize.js";
-
-// // check the health of the application
-// const checkHealth = async (req, res) => {
-//   const length = req.headers['content-length'];
-//   if ((req.method === 'GET' && length > 0) || req.url.includes('?')) {
-//     res.status(400).send();
-//   } else {
-//     try {
-//       await sequelize.authenticate();
-//       res.set('Cache-control', 'no-cache');
-//       res.status(200).send();
-//     } catch (error) {
-//       console.error(error);
-//       res.status(503).send();
-//     }
-//   }else {
-//     // All other request methods
-//     res.status(404).send();
-//   }
-// };
-
-// export default checkHealth;
