22170de43357ea74d1fec1d3bb2427b2c79668a6
altafNEU <shaikh.alt@northeastern.edu>
2023-10-05 12:58:28 -0400

    modified app,csv and dbSetup with comments

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ import express from 'express';
 import appRoute from './routes/app.route.js';
 import assignmentRoute from './routes/assignment.route.js';
 
-const app=express();
-const PORT= process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
-app.use("/healthz",appRoute);
+
+app.use("/healthz", appRoute);
 app.use("/v1/assignments", assignmentRoute);
-app.use("/",(req,res)=>res.status(503).send())
-app.listen(PORT,(err)=>{
-    if(err){
-        console.log("Failed to start the application")
-    }else{
-        console.log("Application running on port number ",PORT);
+
+//  503 status for any other request
+app.use("/", (req, res) => res.status(503).send());
+
+//  Express server
+app.listen(PORT, (err) => {
+    if (err) {
+        console.log("Failed to start the application");
+    } else {
+        console.log("Application running on port number", PORT);
     }
-})
+});
