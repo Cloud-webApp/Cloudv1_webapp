fbf63f095370282edc7489a83186b776ad1bb177
altafNEU <shaikh.alt@northeastern.edu>
2023-10-23 13:22:43 -0400

    merge yml and sequelize js for handling SSL

--- sequelize.js
+++ sequelize.js
@@ -1,17 +1,51 @@
-import dbConfigs from "./config/dbConfig.js";
 import { Sequelize } from "sequelize";
+import dbConfigs from "./config/dbConfig.js";
 
-const sequelize= new Sequelize(
-    dbConfigs.DB,
-    dbConfigs.USER, 
-    dbConfigs.PASSWORD,
-    {
-        host:dbConfigs.HOST,
-        dialect:dbConfigs.dialect,
-        operatorsAliases:false,
-        operatorsAliases: 0,
-        pool:dbConfigs.pool
+// Determine whether to use SSL based on the host
+const isAwsRDS = dbConfigs.HOST && dbConfigs.HOST.includes('.rds.amazonaws.com');
+
+const sequelizeConfig = {
+  dialect: 'postgres',
+  host: dbConfigs.HOST,
+};
+
+if (isAwsRDS) {
+  sequelizeConfig.dialectOptions = {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
+  };
+}
+
+const sequelize = new Sequelize(
+  dbConfigs.DB,
+  dbConfigs.USER,
+  dbConfigs.PASSWORD,
+  sequelizeConfig
 );
 
 export default sequelize;
+
+
+
+// import dbConfigs from "./config/dbConfig.js";
+// import { Sequelize } from "sequelize";
+
+// const sequelize= new Sequelize(
+//     dbConfigs.DB,
+//     dbConfigs.USER, 
+//     dbConfigs.PASSWORD,
+//     {
+//         host:dbConfigs.HOST,
+//         dialect:dbConfigs.dialect,
+//         operatorsAliases:false,
+//         operatorsAliases: 0,
+//         pool:dbConfigs.pool
+//     }
+// );
+
+// export default sequelize;
+
+
+
