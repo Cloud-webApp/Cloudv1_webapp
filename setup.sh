e9525db9e0764a8c1c127f2a5b1ab53b36a5c91c
altafNEU <shaikh.alt@northeastern.edu>
2023-10-23 20:24:12 -0400

    systemd and packer

--- setup.sh
+++ setup.sh
@@ -4,20 +4,28 @@ sudo apt update
 sudo apt upgrade -y
 
 sudo apt install unzip
-
-# Install PostgreSQL and related packages
-sudo apt install -y postgresql postgresql-contrib
-# Start and enable PostgreSQL service
-sudo systemctl start postgresql
-sudo systemctl enable postgresql
 # Install Node.js and npm
 sudo apt install -y nodejs
 sudo apt install -y npm
 # Check Node.js version
 nodejs -v
+
+
+# # Install PostgreSQL and related packages
+# sudo apt install -y postgresql postgresql-contrib
+# # Start and enable PostgreSQL service
+# sudo systemctl start postgresql
+# sudo systemctl enable postgresql
+# # Install Node.js and npm
+# sudo apt install -y nodejs
+# sudo apt install -y npm
+# # Check Node.js version
+# nodejs -v
 # Configure PostgreSQL: set password, create database, and create user
 # sudo -u postgres createuser --interactive --pwprompt postgres
-sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
+
+#sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
+
 # sudo -u postgres createdb altafsmba
 
 
