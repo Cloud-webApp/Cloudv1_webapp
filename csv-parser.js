a785b8ab8a7a56cdb04a0e894626788fe058de5a
altafNEU <shaikh.alt@northeastern.edu>
2023-10-25 13:14:53 -0400

    csv parser and routes

--- csv-parser.js
+++ csv-parser.js
@@ -4,7 +4,7 @@ import db from './dbSetup.js'; // Import your database setup
 import bcrypt from 'bcryptjs';
 
 const insertDataFromCSV = () => {
-  const filePath = 'users.csv'; // Replace with your CSV file path
+  const filePath = './users.csv'; // Replace with your CSV file path
 
   fs.createReadStream(filePath)
     .pipe(csv())
