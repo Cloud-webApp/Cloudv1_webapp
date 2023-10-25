e6c209ef196eafb5c7ea64a1dbe70bfde0f41515
altafNEU <shaikh.alt@northeastern.edu>
2023-10-24 21:34:39 -0400

    user updated details

--- setup.sh
+++ setup.sh
@@ -11,29 +11,29 @@ sudo apt install -y npm
 nodejs -v
 
 
-sudo useradd -m webappuser
-sudo groupadd webappgroup
+# sudo useradd -m webappuser
+# sudo groupadd webappgroup
  
-# Add webappuser and admin to the webappgroup
-sudo usermod -aG webappgroup webappuser
-sudo usermod -aG webappgroup admin
+# # Add webappuser and admin to the webappgroup
+# sudo usermod -aG webappgroup webappuser
+# sudo usermod -aG webappgroup admin
  
-# Set ownership and permissions for webappuser's home directory
-sudo chown -R webappuser:webappgroup /home/webappuser
-sudo chmod -R 750 /home/webappuser
+# # Set ownership and permissions for webappuser's home directory
+# sudo chown -R webappuser:webappgroup /home/webappuser
+# sudo chmod -R 750 /home/webappuser
  
-# Set ownership and permissions for the app.js file in admin's directory
-sudo chown webappuser:webappgroup /home/admin/web-app/app.js
-sudo chmod 750 /home/admin/web-app/app.js
+# # Set ownership and permissions for the app.js file in admin's directory
+# sudo chown webappuser:webappgroup /home/admin/web-app/app.js
+# sudo chmod 750 /home/admin/web-app/app.js
  
  
-# Add webappuser to the systemd-journal group
-sudo usermod -aG systemd-journal webappuser
+# # Add webappuser to the systemd-journal group
+# sudo usermod -aG systemd-journal webappuser
  
-# Set the .env file in admin's directory
-sudo chmod 644 /home/admin/web-app/.env
+# # Set the .env file in admin's directory
+# sudo chmod 644 /home/admin/web-app/.env
  
-# Create the log file and set ownership and permissions
-sudo touch /var/log/webapp.log
-sudo chown webappuser:webappgroup /var/log/webapp.log
-sudo chmod 644 /var/log/webapp.log
+# # Create the log file and set ownership and permissions
+# sudo touch /var/log/webapp.log
+# sudo chown webappuser:webappgroup /var/log/webapp.log
+# sudo chmod 644 /var/log/webapp.log
